8bb4118d856b306b15ac6abe3b7aff5f0db3ff34
Bartłomiej Kwiatkowski <bartekk20@users.noreply.github.com>
2025-11-15 11:04:27 +0100

    zad5

---
+++ zadanie5.py
@@ -0,0 +1,9 @@
+# Napisz kod, który generuje listę liczb zmniejszających się o 4, zaczynając od 80 i kończąc w 0
+
+liczba_poczatkowa = 80
+liczba_o_zmiejszenie = 4
+while liczba_poczatkowa > 0:
+    print(liczba_poczatkowa)
+    liczba_poczatkowa -= liczba_o_zmiejszenie
+
+
