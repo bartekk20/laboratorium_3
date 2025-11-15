ab9f27bc5775fcd9a242cb9815abc0ee53d39892
Bartłomiej Kwiatkowski <bartekk20@users.noreply.github.com>
2025-11-15 11:26:17 +0100

    .

--- zadanie8.py
+++ zadanie8.py
@@ -8,3 +8,48 @@ print(txt[::2])
 print(txt[1::3])
 print(txt[10:])
 print(txt[::-1])
+
+# #zadanie 8
+# text = ("Python jest super")
+#
+# print(text[0])
+# print(text[-1])
+# print(text[0::2])
+# print(text[1::3])
+# print(text[10::1])
+# print(text[::-1])
+
+
+# zad 9
+# Szymon Madeja 7417811:24
+# #Wczyta (zmienną) imię oraz wyświetli tekst „Witaj” oraz wczytane imię.
+# imie = input("Podaj imie: ")
+#
+# print(f"Witaj {imie}")
+#
+# wiek = int(input("Podaj wiek: "))
+#
+# print(f"Twój wiek to: {wiek}")
+#
+# nazwisko = input("Podaj nazwisko:")
+#
+# print(imie[0], nazwisko[0])
+#
+# x = input("Podaj tekst: ")
+#
+# for i in range(5):
+#     print(x)
+#
+# z = input("Podaj 2 tekst: ")
+#
+# trzeci = x,z
+#
+# print(trzeci)
+#
+# czwarty = imie,nazwisko
+#
+# dlugosc = len(czwarty)
+#
+# polowa = round((dlugosc / 2),0)
+#
+# print(czwarty[0,polowa])
